653dfb8b0fe114bf7221a9b29001495a35123dcd
Gabriel Mello <63933270+Mello-Gabriel@users.noreply.github.com>
2021-06-21 22:09:44 -0300

    sprint5

--- progama/funcao.py
+++ progama/funcao.py
@@ -1,33 +1,40 @@
+#%%
 import pandas as pd
 from googlesearch import search
 import requests
 import PyPDF2
 from wordcloud import WordCloud, STOPWORDS
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
+from math import sqrt
+
 # %%
 def nome_empresas():
     with open(r'..\ref\empresas.txt','r', encoding='utf-8') as texto:
-        global empresas
         empresas=texto.readlines()  
         empresas=[x.replace('\n','').lower() for x in empresas]
+        return empresas
 # %%
-def pegar_links(ano='2019', numero='10'):
-    global links
+def pegar_links(empresas, ano='2019', numero='10'):
     links={}
-    for empresa in empresas:
-        links[empresa]=pd.DataFrame(search(f'{empresa} relatório sustentabilidade {ano} filetype:pdf', num_results=10))
-    links=pd.concat(links).droplevel(1)
+    if type(empresas) == list:
+        for empresa in empresas:
+            links[empresa]=pd.DataFrame(search(f'{empresa} relatório sustentabilidade {ano} filetype:pdf', num_results=10))
+    else:
+        links[empresas]=pd.DataFrame(search(f'{empresas} relatório sustentabilidade {ano} filetype:pdf', num_results=10))
+    return links
 # %%
-def pegar_pdfs():
+def pegar_pdfs(empresas, links):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
     for empresa in empresas:
         try:
-            r=requests.get(f"{links[empresa][0]}", allow_redirects=True, headers=headers)
+            r=requests.get(f"{links[empresa][0][0]}", allow_redirects=True, headers=headers)
             open(f'../pdf_files/{empresa}.pdf','wb').write(r.content)
         except:
             print(f"Não foi possível baixar o link {links[empresa][0]}")
 #%%
-def pdf_to_text(pdf_path):
-    pdf_file=open(f'{pdf_path}', 'rb')
+def pdf_to_text(caminho):
+    pdf_file=open(caminho, 'rb')
     pdf_reader=PyPDF2.PdfFileReader(pdf_file)
     num_pag=pdf_reader.numPages
     dici={}
@@ -49,7 +56,7 @@ def pdf_to_text(pdf_path):
             pdf_text=pdf_text.replace(x,y)
         dici[page]=pdf_text
     lista="".join(dici.values())
-    nome_arquivo=pdf_path.split("\\")[-1].split('.')[-2]
+    nome_arquivo=caminho.split("/")[-1].split('.')[-2]
     f=open(f'../text_files/{nome_arquivo}.txt','w', encoding='utf-8')
     f.write(lista)
     f.close()    
@@ -70,5 +77,49 @@ def wordcloud(text_path):
     palavras.sort_values(ascending=False, by=0,inplace=True)
     palavras.reset_index(inplace=True)
     palavras.columns=('palavras','quantidade')
-    nome_arquivo=text_path.split("\\")[-1].split('.')[-2]
+    nome_arquivo=text_path.split("/")[-1].split('.')[-2]
     palavras.to_csv(f'../tabela_palavras/{nome_arquivo}.csv')
+# %%
+def sim_euclidiana(avaliacoes, c1, c2):
+    mesmos_filmes = {}
+    
+    for filme in avaliacoes[c1]:
+        if filme in avaliacoes[c2]:
+            mesmos_filmes[filme] = 1
+    
+    if len(mesmos_filmes) == 0: return 0
+    
+    dist_euclidiana = sqrt(sum([pow((avaliacoes[c1][filme] - avaliacoes[c2][filme]),2) for filme in mesmos_filmes]))
+    
+    return 1 / (1 + dist_euclidiana)
+#%%
+def top_similares(avaliacoes, pessoa, n = 1, medida = sim_euclidiana):
+    valores = [(medida(avaliacoes, pessoa, outra), outra) for outra in avaliacoes if outra != pessoa]
+    valores.sort()
+    valores.reverse()
+    return valores[0:n]
+#%%
+def indicar_palavra(avaliacoes,pessoa):
+    pessoa_similar=top_similares(avaliacoes, pessoa, 1)[0][1]
+    recomendado=[]
+    filme_pessoa=list(avaliacoes[pessoa].keys())
+    filme_pessoa_similar=list(avaliacoes[pessoa_similar].keys())
+    for filme in filme_pessoa_similar:
+        if filme not in filme_pessoa:
+            recomendado.append(filme)
+    avaliacoes_df=pd.DataFrame.from_dict(avaliacoes)
+    avaliacoes_df_recomendado=avaliacoes_df[avaliacoes_df.index.isin(recomendado)]
+    avaliacoes_df_recomendado['sum']=avaliacoes_df_recomendado.sum(axis=1)
+    return avaliacoes_df_recomendado['sum'].idxmax()
+#%%
+def matriz_similaridade(avaliacoes):
+    matriz={}
+    for usuariox in avaliacoes.keys():
+        matriz[usuariox]={}
+        for usuarioy in avaliacoes.keys():
+            matriz[usuariox][usuarioy]=sim_euclidiana(avaliacoes,usuariox,usuarioy)
+    matriz_df=pd.DataFrame.from_dict(matriz)
+    np_matriz=matriz_df.to_numpy()
+    Z = hierarchy.linkage(np_matriz, 'single')
+    plt.figure()
+    dn = hierarchy.dendrogram(Z, labels=list(matriz_df.columns), leaf_rotation=90)
--- progama/script.py
+++ progama/script.py
@@ -1,20 +1,42 @@
 #%%
 import glob
-from funcao import nome_empresas, pdf_to_text, pegar_links, pegar_pdfs, wordcloud
+from funcao import nome_empresas, pdf_to_text, pegar_links, pegar_pdfs, top_similares, indicar_palavra, wordcloud, sim_euclidiana, matriz_similaridade
+import pandas as pd
+#%%
+empresas=nome_empresas()
+pdfs=[pdf.split('\\')[-1].split('.')[0] for pdf in glob.glob(r'..\pdf_files\*.pdf')]
+textos=[texto.split('\\')[-1].split('.')[0] for texto in glob.glob(r'..\text_files\*.txt')]
+tabelas=[tabela.split('\\')[-1].split('.')[0] for tabela in glob.glob(r'..\tabela_palavras\*.csv')]
+empresas_sem_pdf=[empresa for empresa in empresas if empresa not in pdfs]
+empresas_sem_texto=[empresa for empresa in empresas if empresa not in textos]
+empresas_sem_tabela=[empresa for empresa in empresas if empresa not in tabelas]
+#%%
+if len(empresas_sem_pdf) > 0:
+    links=pegar_links(empresas_sem_pdf)
+    pegar_pdfs(empresas_sem_pdf, links)
+#%%
+if len(empresas_sem_texto) >0:
+    caminhos=[glob.glob(fr'../pdf_files/{empresa}.pdf') for empresa in empresas_sem_texto]
+    [pdf_to_text(caminho[0]) for caminho in caminhos]
 
 #%%
-pdfs=glob.glob('..\pdf_files\*.pdf')
-if len(pdfs) == 0:
-    nome_empresas()
-    pegar_links()
-    pegar_pdfs()
-    [pdf_to_text(pdf) for pdf in pdfs]
-    text=glob.glob('../text_files/*.*')
-    [wordcloud(text) for file in text]
+if len(empresas_sem_tabela) >0:
+    textos=[glob.glob(fr'../text_files/{empresa}.txt') for empresa in empresas_sem_tabela]
+    [wordcloud(text[0]) for text in textos]
+# %%
+dicio_palavras={}
+palavras=glob.glob(r'..\tabela_palavras\*.csv')
+for palavra in palavras:
+    ler_tabela=pd.read_csv(palavra, index_col=1).drop(columns='Unnamed: 0')
+    ler_tabela=ler_tabela.to_dict('dict')
+    dicio_palavras[palavra.split('\\')[-1].split('.')[0]]=ler_tabela['quantidade']
+# %%
+sim_euclidiana(dicio_palavras,'weg','ccr')
+# %%
+matriz_similaridade(dicio_palavras)
+# %%
+top_similares(dicio_palavras,'ccr', n=2)
 
-else:
-    text=glob.glob('../text_files/*.*')
-    [pdf_to_text(pdf) for pdf in pdfs if len(text)==0]
-    tabelas=glob.glob('../tabela_palavras/*.*')
-    [wordcloud(file) for file in text if len(tabelas)==0]
+# %%
+indicar_palavra(dicio_palavras,'weg')
 # %%
