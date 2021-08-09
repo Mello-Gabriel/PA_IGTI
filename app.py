d36797ec50190300fac59266ef4bcefe466cf6f9
Gabriel Mello <63933270+Mello-Gabriel@users.noreply.github.com>
2021-08-08 22:12:12 -0300

    a

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ st.sidebar.write('Esse é um aplicativo web desenvolvido para conclusão do curs
 st.sidebar.write('Eu sou Gabriel de Mello, me conheça melhor em [LinkedIn](https://www.linkedin.com/in/gabriel-guimaraes-de-mello/).')
 st.sidebar.write('Esse projeto foi escrito em Python e está disponível no [Github](https://github.com/Mello-Gabriel/PA_IGTI).')
 
+st.write(sim_euclidiana(palavras, empresa,empresa2))
 
 col1, col2 = st.columns(2)
 with col1:
@@ -34,8 +35,8 @@ with col1:
     tabela=pd.read_csv(fr'tabela_palavras/{empresa}.csv', index_col=1).sort_values(by='quantidade',ascending=False)[:20]
     fig= px.histogram(tabela, x=tabela.index, y='quantidade')
     st.plotly_chart(fig)
-    top1=top_similares(palavras, empresa, 1)
-    st.write("A empresa que possui o relatório mais similar é:", top1[0][1])
+    top1=top_similares(palavras, empresa)
+    st.write("A empresa que possui o relatório mais similar é:", top1[0][1].upper())
     st.write(f'Faça o download do relatório de sustentabilidade da {empresa.upper()}  aqui: [link](https://raw.githubusercontent.com/Mello-Gabriel/PA_IGTI/master/pdf_files/{empresa.replace(" ","%20")}.pdf)')
 
 
@@ -47,6 +48,8 @@ with col2:
     tabela=pd.read_csv(fr'tabela_palavras/{empresa2}.csv', index_col=1).sort_values(by='quantidade',ascending=False)[:20]
     fig= px.histogram(tabela, x=tabela.index, y='quantidade')
     st.plotly_chart(fig)
-    top2=top_similares(palavras, empresa2, 1)
-    st.write("A empresa que possui o relatório mais similar é:", top2[0][1])
+    top2=top_similares(palavras, empresa2)
+    st.write("A empresa que possui o relatório mais similar é:", top2[0][1].upper())
     st.write(f'Faça o download do relatório da sustentabilidade da {empresa2.upper()} aqui: [link](https://raw.githubusercontent.com/Mello-Gabriel/PA_IGTI/master/pdf_files/{empresa2.replace(" ","%20")}.pdf)')
+
+st.write(sim_euclidiana(palavras, empresa,empresa2))
