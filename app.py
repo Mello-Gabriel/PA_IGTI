48b3922a7affa51ca2c8da927cfa55ae196d306b
Gabriel Mello <63933270+Mello-Gabriel@users.noreply.github.com>
2021-08-15 10:25:59 -0300

    update app.py

--- app.py
+++ app.py
@@ -31,11 +31,13 @@ st.sidebar.write(
 st.sidebar.write(
     'Esse projeto foi escrito em Python e está disponível no [Github](https://github.com/Mello-Gabriel/PA_IGTI).')
 
+#seção para o aplicativo do PowerBI embeded
 with st.expander('NAVEGADOR DE PALAVRAS E EMPRESAS'):
-    st.write('Utilize essa rede de relacionamento para xplorar as relações entre as empresas através das palavras de seus relatórios. As empresas são representadas pelos pontos coloridos e as palavras pelos pontos cinzas')
+    st.write('Utilize essa rede de relacionamento para explorar as relações entre as empresas através das palavras de seus relatórios. As empresas são representadas pelos pontos coloridos e as palavras pelos pontos cinzas')
     st.write('Experimente utilizar a caixa da direta para buscar palavras relavantes e identificar as empresas que as utilizaram no relatório')
     components.iframe('https://app.powerbi.com/view?r=eyJrIjoiMWNiMzllYjktYzU5OC00NzM0LWIzYzEtNzRhYzViNDk0MDM3IiwidCI6IjYxYzM2ZWZhLTAwY2UtNDIyMi1iY2VmLWM3MmRiNTZhYWM0NyJ9&pageName=ReportSection', height=720)
 
+#seção com duas colunas, para apresentar as empresas comparadas.
 col1, col2 = st.columns(2)
 with col1:
     # apresentar o WordCloud da empresa selecionada
@@ -45,9 +47,11 @@ with col1:
     tabela = pd.read_csv(fr'tabela_palavras/{empresa}.csv', index_col=1).sort_values(by='quantidade', ascending=False)[:20]
     fig = px.histogram(tabela, x=tabela.index, y='quantidade')
     st.plotly_chart(fig)
+    #apresentar a empresa com maior similaridade
     top1 = top_similares(palavras, empresa)
     st.write("A empresa que possui o relatório mais similar é:",
             top1[0][1].upper())
+    #apresentar o link para download do relatório
     st.write(f'Faça o download do relatório de sustentabilidade da {empresa.upper()}  aqui: [link](https://raw.githubusercontent.com/Mello-Gabriel/PA_IGTI/master/pdf_files/{empresa.replace(" ","%20")}.pdf)')
 
 with col2:
@@ -59,9 +63,11 @@ with col2:
         by='quantidade', ascending=False)[:20]
     fig = px.histogram(tabela, x=tabela.index, y='quantidade')
     st.plotly_chart(fig)
+    #apresentar a empresa com maior similaridade
     top2 = top_similares(palavras, empresa2)
     st.write("A empresa que possui o relatório mais similar é:",
             top2[0][1].upper())
+    #apresentar o link para download do relatório.
     st.write(
         f'Faça o download do relatório da sustentabilidade da {empresa2.upper()} aqui: [link](https://raw.githubusercontent.com/Mello-Gabriel/PA_IGTI/master/pdf_files/{empresa2.replace(" ","%20")}.pdf)'
             )
